a9567315f8bcf42e8324af3edcdc8f28fbdc595b
97hackbrian <brayandurantoconas@gmail.com>
2026-01-13 13:05:03 -0400

    3 containers

--- launch/unet_laneseg.launch.py
+++ launch/unet_laneseg.launch.py
@@ -7,56 +7,84 @@ def generate_launch_description():
     # --- CONFIGURACIÓN ---
     CAMERA_WIDTH = 640
     CAMERA_HEIGHT = 480
+    NETWORK_WIDTH = 256
+    NETWORK_HEIGHT = 256
     
     INPUT_TENSOR = 'input_0'
     OUTPUT_TENSOR = 'output_0'
     ENGINE_PATH = '/workspaces/isaac_ros-dev/ros2/src/qcar2_LaneSeg-ACC/models/unet/lane_unet.plan'
 
-    # --- CONTENEDOR 1: PREPROCESAMIENTO (Cámara -> Encoder) ---
+    # ---------------------------------------------------------
+    # CONTENEDOR 1: SOLO REDIMENSIONADO (El Portero)
+    # ---------------------------------------------------------
+    resize_container = ComposableNodeContainer(
+        name='resize_container',
+        namespace='',
+        package='rclcpp_components',
+        executable='component_container', # Estándar
+        composable_node_descriptions=[
+            ComposableNode(
+                package='isaac_ros_image_proc',
+                plugin='nvidia::isaac_ros::image_proc::ResizeNode',
+                name='image_resize',
+                parameters=[{
+                    'output_width': NETWORK_WIDTH,
+                    'output_height': NETWORK_HEIGHT,
+                    'num_blocks': 40
+                }],
+                remappings=[
+                    ('image', '/camera/color_image'),
+                    ('resize/image', '/image_resized'),
+                    ('camera_info', '/camera/camera_info')
+                ]
+            )
+        ],
+        output='screen'
+    )
+
+    # ---------------------------------------------------------
+    # CONTENEDOR 2: SOLO ENCODER (El Traductor)
+    # ---------------------------------------------------------
     encoder_container = ComposableNodeContainer(
         name='encoder_container',
         namespace='',
         package='rclcpp_components',
-        executable='component_container', # Estándar (Seguro)
+        executable='component_container', # Estándar
         composable_node_descriptions=[
-            # 1. Convertidor
-            ComposableNode(
-                package='isaac_ros_image_proc',
-                plugin='nvidia::isaac_ros::image_proc::ImageFormatConverterNode',
-                name='image_converter',
-                parameters=[{'encoding_desired': 'rgb8', 'image_width': CAMERA_WIDTH, 'image_height': CAMERA_HEIGHT}],
-                remappings=[('image_raw', '/camera/color_image'), ('image', '/image_clean')]
-            ),
-            # 2. Encoder
             ComposableNode(
                 package='isaac_ros_dnn_image_encoder',
                 plugin='nvidia::isaac_ros::dnn_inference::DnnImageEncoderNode',
                 name='dnn_image_encoder',
                 parameters=[{
-                    'input_image_width': CAMERA_WIDTH,
-                    'input_image_height': CAMERA_HEIGHT,
-                    'network_image_width': 256,
-                    'network_image_height': 256,
+                    'input_image_width': NETWORK_WIDTH,
+                    'input_image_height': NETWORK_HEIGHT,
+                    'network_image_width': NETWORK_WIDTH,
+                    'network_image_height': NETWORK_HEIGHT,
                     'image_mean': [0.485, 0.456, 0.406],
                     'image_stddev': [0.229, 0.224, 0.225],
                     'tensor_output_order': 'NCHW',
                     'tensor_name': INPUT_TENSOR, 
                     'num_blocks': 40
                 }],
-                remappings=[('image', '/image_clean'), ('encoded_tensor', '/tensor_input')]
+                remappings=[
+                    ('image', '/image_resized'),
+                    ('encoded_tensor', '/tensor_input')
+                ]
             )
         ],
         output='screen'
     )
 
-    # --- CONTENEDOR 2: INFERENCIA (TensorRT + Decoder) ---
+    # ---------------------------------------------------------
+    # CONTENEDOR 3: INFERENCIA (TensorRT + Decoder)
+    # ---------------------------------------------------------
     inference_container = ComposableNodeContainer(
         name='inference_container',
         namespace='',
         package='rclcpp_components',
-        executable='component_container', # Estándar (Seguro)
+        executable='component_container', # Estándar
         composable_node_descriptions=[
-            # 3. TensorRT
+            # TensorRT
             ComposableNode(
                 package='isaac_ros_tensor_rt',
                 plugin='nvidia::isaac_ros::dnn_inference::TensorRTNode',
@@ -70,9 +98,12 @@ def generate_launch_description():
                     'verbose': False,
                     'force_engine_update': False
                 }],
-                remappings=[('tensor_sub', '/tensor_input'), ('tensor_pub', '/tensor_output')]
+                remappings=[
+                    ('tensor_sub', '/tensor_input'),
+                    ('tensor_pub', '/tensor_output')
+                ]
             ),
-            # 4. Decoder
+            # Decoder
             ComposableNode(
                 package='isaac_ros_unet',
                 plugin='nvidia::isaac_ros::unet::UNetDecoderNode',
@@ -80,14 +111,17 @@ def generate_launch_description():
                 parameters=[{
                     'network_output_type': 'sigmoid',
                     'color_segmentation_mask_encoding': 'rgb8',
-                    'mask_width': 256,
-                    'mask_height': 256,
+                    'mask_width': NETWORK_WIDTH,
+                    'mask_height': NETWORK_HEIGHT,
                     'color_palette': [0, 0, 0, 0, 255, 0]
                 }],
-                remappings=[('tensor_sub', '/tensor_output'), ('unet/raw_segmentation_mask', '/lane_detection/mask')]
+                remappings=[
+                    ('tensor_sub', '/tensor_output'),
+                    ('unet/raw_segmentation_mask', '/lane_detection/mask')
+                ]
             )
         ],
         output='screen'
     )
 
-    return launch.LaunchDescription([encoder_container, inference_container])
+    return launch.LaunchDescription([resize_container, encoder_container, inference_container])
